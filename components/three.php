aa3be07192aabedd5a3b8d56d4de7ced3b59cbfe
ekaterinagp <katya.koriakina@gmail.com>
2019-08-29 21:53:19 +0200

    components added

---
+++ components/three.php
@@ -0,0 +1,11 @@
+
+
+<?php
+$pageTitle='THREE';
+$sScriptName="three.js";
+require_once(__DIR__.'/components/top.php');
+?>
+<h1>Three</h1>
+<?php
+require_once(__DIR__.'/components/footer.php');
+?>
---
+++ components/two.php
@@ -0,0 +1,13 @@
+
+
+<?php
+$pageTitle='TWO';
+$sScriptName="two.js";
+
+require_once(__DIR__.'/components/top.php');
+?>
+<h1>Two</h1>
+
+<?php
+require_once(__DIR__.'/components/footer.php');
+?>
