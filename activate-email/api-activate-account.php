8c6c2e36a7eacebce3de4f22d19f426e8f4ff5e2
ekaterinagp <katya.koriakina@gmail.com>
2019-09-05 20:56:30 +0200

    activate works weird, to check

--- activate-email/api-activate-account.php
+++ activate-email/api-activate-account.php
@@ -1,23 +1,24 @@
 <?php
+$sKey = $_GET['key']; // key from the url
+$sUserId = $_GET['id']; // user id from the url
+echo "<h1>Welcome {$_GET['id']} $sKey</h1>";
 
+$sjData = file_get_contents('data.json'); // open file
+$jData = json_decode($sjData); // convert text to object
 
-
-
-$sKey = $_GET['key'];
-$sUserID = $_GET['id'];
-echo "<h1>Welcome {$sUserID}</h1>";
-
-$sjData = file_get_contents('data.json');
-$jData = json_decode($sjData);
-if ($jData->$sUserID->verified == 1) {
-  echo "You can not activate again";
-  ecit;
+if ($jData->$sUserId->verified == 1) {
+  echo 'Cannot activate account';
+  exit;
 }
-if ($jData->$sUserID->activationKey == $sKey) {
+
+
+
+
+if ($jData->$sUserId->activationKey == $sKey) {
   echo 'match found';
-  $jData->$sUserID->verified = 1;
-  $sjData = json_encode($jData);
-  file_put_contents('data.json', $sjData);
+  $jData->$sUserId->verified = 1; // convert 0 to 1
+  $sjData = json_encode($jData, JSON_PRETTY_PRINT); // convert json to text
+  file_put_contents('data.json', $sjData);  // save text to file
 } else {
   echo 'no match found';
 }
