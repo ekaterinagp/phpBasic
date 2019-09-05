26e7f78db578d6bb7cb5a7bac5a4e41b1b5c22aa
ekaterinagp <katya.koriakina@gmail.com>
2019-09-05 12:51:34 +0200

    sending emails fixed

--- activate-email/api-send-email.php
+++ activate-email/api-send-email.php
@@ -16,12 +16,19 @@ $mail = new PHPMailer(true);
 
 try {
   //Server settings
+  $mail->SMTPOptions = array(
+    'ssl' => array(
+      'verify_peer' => false,
+      'verify_peer_name' => false,
+      'allow_self_signed' => true
+    )
+  );
   $mail->SMTPDebug = 2;                                       // Enable verbose debug output
   $mail->isSMTP();                                            // Set mailer to use SMTP
   $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
   $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
   $mail->Username   = 'wkea108@gmail.com';                     // SMTP username
-  $mail->Password   = '108108kea';                               // SMTP password
+  $mail->Password   = 'kea787878';                               // SMTP password
   $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
   $mail->Port       = 587;                                    // TCP port to connect to
 
--- activate-email/regex.php
+++ activate-email/regex.php
@@ -1,6 +1,14 @@
 <?php
-$pattern = "/ca[kf]e/";
-$text = "He was eating cake in the cafe.";
+// $pattern = "/ca[kf]e/";
+// $text = "He was eating cake in the cafe.";
+// if (preg_match($pattern, $text)) {
+//   echo "Match found!";
+// } else {
+//   echo "Match not found.";
+// }
+
+$pattern = "/P\d{4}$/";
+$text = "P11111";
 if (preg_match($pattern, $text)) {
   echo "Match found!";
 } else {
